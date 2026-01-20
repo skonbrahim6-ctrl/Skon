480b27d5b6b7eb297ad865b0e42c273f42eef9f3
WORMAI.co. <skonbrahim6@gmail.com>
2026-01-20 21:37:17 +0100

    Update call.py

--- utils/call.py
+++ utils/call.py
@@ -1,12 +1,17 @@
-from pytgcalls import PyTgCalls
-from pytgcalls.types import AudioPiped
 from yt_dlp import YoutubeDL
 
-# إعدادات استخراج الصوت من يوتيوب
-ytdl_opts = {"format": "bestaudio/best", "quiet": True, "no_warnings": True}
+ytdl_opts = {
+    "format": "bestaudio/best",
+    "quiet": True,
+    "no_warnings": True,
+    "default_search": "auto",
+    "source_address": "0.0.0.0",
+}
+
 ytdl = YoutubeDL(ytdl_opts)
 
 def get_audio_link(query):
-    info = ytdl.extract_info(f"ytsearch:{query}", download=False)['entries'][0]
-    return info['url'], info['title']
-  
+    search_query = f"ytsearch:{query}"
+    info = ytdl.extract_info(search_query, download=False)['entries'][0]
+    return info['url'], info['title'], info['duration'], info['id']
+    
