0b2a65979bc5146440772b01f537e16c229dc6b0
WORMAI.co. <skonbrahim6@gmail.com>
2026-01-20 21:24:24 +0100

    Create queue.py

---
+++ utils/queue.py
@@ -0,0 +1,17 @@
+queue = {}
+
+def add_to_queue(chat_id, title, url, user):
+    if chat_id not in queue:
+        queue[chat_id] = []
+    queue[chat_id].append({"title": title, "url": url, "user": user})
+    return len(queue[chat_id])
+
+def get_queue(chat_id):
+    if chat_id in queue:
+        return queue[chat_id]
+    return []
+
+def clear_queue(chat_id):
+    if chat_id in queue:
+        queue.pop(chat_id)
+      
