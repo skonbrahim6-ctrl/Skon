41f24a10869295f35edb1f7939d0ec52a53d9353
WORMAI.co. <skonbrahim6@gmail.com>
2026-01-20 21:09:12 +0100

    Create config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_ID = int(os.getenv("API_ID", "31840536"))
+API_HASH = os.getenv("API_HASH", "10cd5cbb6279fc6f77494a5f0fa01631")
+BOT_TOKEN = "8584176399:AAFXwo50vkJd802-dWBdIJeyPOr_2p3cCdw"
+SESSION = os.getenv("SESSION", "BAHl2RgARvcc5HoCIgc8N0R59LJZXrIaOOfv9O19XlTg4Cnzo-kLqfjoK1quOQoCdXpvO4_vZzg6AVrd5Y2maa5UR_IuvWu0X4-3PbsM6FTyHE_3MQ4yDwR_932RYUXcQALI4ZqPrSuCNCiQAUTudk_bjwJ1nbnFFqXdAITWubhuQCpwFzadnyRVkfWOHltWy6WFbRGgxfT7r5oTDB5o6c5OtqE1sa0hj_ODgijs8KwF4_vSUDqQ57KzIqqug4_v4JZb9_7G0EjmOGekRtF68hhnnVr-u9JAf0orfI_N57jmNJFdAoBDe2_lyZZ3iabM-arwBsCzpddD0BIF-BDSxOO2OrJ1IQAAAAHCIlQpAA")
+OWNER_ID = 7445763567 # آيديك
+SUPPORT_USER = "@C_R_B_X"
+BOT_USERNAME = "spoti_music_bot"
+START_VIDEO = "https://l.top4top.io/m_36723i2xy0.mp4"
