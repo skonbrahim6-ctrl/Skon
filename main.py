372b96195236512cb203492be7a3ca97b3e451dd
WORMAI.co. <skonbrahim6@gmail.com>
2026-01-21 02:01:06 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,17 +2,10 @@ import asyncio
 from pyrogram import Client, filters
 from config import API_ID, API_HASH, BOT_TOKEN
 
-# تشغيل البوت فقط بدون محرك المكالمات
-app = Client(
-    "SkonBot",
-    api_id=API_ID,
-    api_hash=API_HASH,
-    bot_token=BOT_TOKEN
-)
+app = Client("SkonBot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
 @app.on_message(filters.command("start"))
 async def start(client, message):
-    await message.reply_text("✅ تم إلغاء نظام الأغاني مؤقتاً.. البوت يعمل الآن كبوت رسائل عادي في تيرميكس!")
+    await message.reply_text("✅ البوت شغال الآن في تيرميكس بدون نظام المكالمات!")
 
-print("🚀 جاري تشغيل البوت بدون نظام المكالمات...")
 app.run()
